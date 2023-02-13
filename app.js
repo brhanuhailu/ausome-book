4ec659322c94d9c187b511940f6f7b06d1642557
Daniel <dselasea@gmail.com>
2023-02-13 10:56:37 +0000

    Add addBook function and deleteBook function

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const books = {
-  title: '',
-  author: '',
+const add = document.querySelector('#add');
+const books = []
+
+function addBook(title, author) {
+  books.push({title, author});
+  return books;
 }
+
+function deleteBook(){
+
+}
+
+console.log(addBook("Lord of the rings", "I wrote it"));
+console.log(addBook("Harry Potter", "J.K Rowling"));
 
