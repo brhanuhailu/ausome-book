0011253ade0a7e9c48a800b59dffc6fb64c61b03
Daniel <dselasea@gmail.com>
2023-02-14 16:12:44 +0000

    add styles

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-use-before-define */
 const listOfbooks = document.querySelector('.books-content');
+const newB = document.querySelector('.new-books');
+
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -26,9 +28,12 @@ class StoreBook {
   removeBook(bookid) {
     const rmvbook = document.getElementById(bookid);
     rmvbook.remove();
-    removeSuccess();
     this.BookData = this.BookData.filter((x) => x.bookid !== bookid);
     localStorage.setItem('BookDB', JSON.stringify(this.BookData));
+    removeSuccess();
+    if (this.BookData.length < 1) {
+      listOfbooks.classList.add('no-border');
+    }
   }
 }
 
@@ -66,46 +71,48 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 // Add Button
 const addnewBook = document.querySelector('#add');
-addnewBook.addEventListener('click', () => {
+addnewBook.addEventListener('click', (e) => {
   if (title.value === '' || author.value === '') {
     showAlert();
+    e.preventDefault();
   } else {
     const item = getformInput();
     savebook.addBook(item);
     success();
+    e.preventDefault();
   }
 });
 
 // display
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', function() {
   savebook.BookData = JSON.parse(localStorage.getItem('BookDB' || '[]'));
   if (savebook.BookData === null) {
     savebook.BookData = [];
     return;
   }
   savebook.BookData.forEach((item) => DisplayBooks(item));
-};
+});
 
 function showAlert() {
   const div = document.createElement('div');
   div.className = 'error';
-  div.appendChild(document.createTextNode('fields are required'));
-  const newB = document.querySelector('.new-books');
   newB.appendChild(div);
+  div.appendChild(document.createTextNode('Fields are required'));
   setTimeout(() => document.querySelector('.error').remove(), 3000);
 }
 
 function success() {
   const div = document.createElement('div');
   div.className = 'success';
+  newB.append(div);
   div.appendChild(document.createTextNode('Book successfully added!'));
-  const newB = document.querySelector('.new-books');
-  newB.appendChild(div);
   setTimeout(() => document.querySelector('.success').remove(), 3000);
 }
 
 function removeSuccess() {
   const div = document.createElement('div');
   div.className = 'remove-book';
+  newB.append(div);
   div.appendChild(document.createTextNode('Book Removed Successfully!'));
+  setTimeout(() => document.querySelector('.remove-book').remove(), 3000);
 }
